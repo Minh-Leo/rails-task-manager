e3c7a21e1af34cdea3352bda120149d65b2378a1
minh bui <tranduyminh.bui@gmail.com>
2020-02-18 12:41:30 +1100

    Add routes/views for CRUD tasks

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,4 +1,46 @@
+# Task controller
 class TaskController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :mark_completed]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
   def create
+    @task = Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  def edit; end
+
+  def update
+    @task.update(task_params)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  def mark_completed
+    @task.completed = !@task.completed
+    @task.update
+  end
+
+  # private from here
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  get 'task/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'tasks', to: 'task#index', as: :tasks
+
+  get 'tasks/new', to: 'task#new'
+  post 'tasks', to: 'task#create'
+
+  get 'tasks/:id/edit', to: 'task#edit'
+  patch 'task/:id', to: 'task#update'
+
+  get 'tasks/:id', to: 'task#show', as: :task
+
+  delete 'tasks/:id', to: 'task#destroy'
 end
